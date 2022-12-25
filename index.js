4cf5ddc22cc216676b5719797aeae759e26fd0ee
fahadamin646 <fahadamin289@gmail.com>
2022-12-25 18:35:27 +0500

    firstCommit

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const multer = require('multer');
+var cors = require('cors')
+
+const app = express();
+
+
+app.use(cors())
+// Set up Multer to handle file uploads and save them to a designated folder
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, 'uploads/');
+    },
+    filename: (req, file, cb) => {
+        cb(null, file.originalname);
+    }
+});
+const upload = multer({ storage });
+
+// Set up the file upload route
+app.post('/upload', upload.single('file'), (req, res) => {
+    // Return a response with the link to the uploaded file
+    res.send({ fileUrl: `http://localhost:3000/uploads/${req.file.originalname}` });
+});
+
+app.get('/getResponse', (req, res) => {
+    res.send({ message: "Hello Fahad" });
+})
+app.listen(3000, () => {
+    console.log('Server listening on port 3000');
+});
